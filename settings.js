1db5a8692e95bf0a10cb2bde89ae5ada9dfc0926
RUSH-X <yeshulahumansa@gmail.com>
2025-06-03 09:55:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~cmUjFSJK#tG0iJsuE1-6ZgPigN1CEO_qzZUts90up5keXIcl7Unk' 
 //Enter your KANGO-XMD session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
@@ -24,7 +24,7 @@ global.botname = process.env.BOT_NAME || 'KANGO-XMD'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '+94713713687' 
 
 //--------------------[ SUDO ]--------------------------//
 
